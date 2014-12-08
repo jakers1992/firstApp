f0674d773a5792721ef182fa2f46552723774fd0
jonak_000 <jonakers2010@gmail.com>
2014-12-08 12:50:12 -0600

    First commit

---
+++ src/com/one/firstapp/Main.java
@@ -0,0 +1,67 @@
+package com.one.firstapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+
+public class Main extends Activity {
+	
+	int counter;
+	Button add, sub;
+	TextView display;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		counter = 0;
+		add = (Button) findViewById(R.id.bAdd);
+		sub = (Button) findViewById(R.id.bSub);
+		display = (TextView) findViewById(R.id.tvDisplay);
+		Log.i("out", "onCreate called");
+		
+	}
+	
+	public void addOne(View v)
+	{
+		counter++;
+		display.setText("Your total is " +counter);
+	}
+	
+	public void subOne(View v)
+	{
+		counter--;
+		display.setText("Your total is " +counter);
+	}
+
+	public void radioClicked(View v)
+	{
+		RadioButton rButton = (RadioButton) findViewById
+				(R.id.radio);
+		if(rButton.isChecked())
+			rButton.setChecked(false);
+		else rButton.setChecked(true);
+	}
+	
+	
+	public void onDestroy()
+	{
+		super.onDestroy();
+		Log.i("out", "onDestroy called");
+	}
+}
+
+/*
+ * add.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});*/
---
+++ src/com/one/firstapp/Splash.java
@@ -0,0 +1,48 @@
+package com.one.firstapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+public class Splash extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		Log.i("out", "Splash created");
+		setContentView(R.layout.splash);
+		Thread timer = new Thread()
+		{
+			public void run()
+			{
+				try
+				{
+					Log.i("out", "Splash image start");
+					sleep(2000);
+				} 
+				catch(InterruptedException e)
+				{
+					e.printStackTrace();
+					Log.i("out", "Thread interrupted");
+				}
+				finally
+				{
+					Intent openMain = new Intent(Splash.this, Main.class);
+					startActivity(openMain);
+					finish();
+				}	
+			}
+			
+		};
+		timer.start();
+	
+	}
+	/*@Override
+	protected void onPause(){
+		super.onPause();
+		finish();
+	}*/
+
+}
